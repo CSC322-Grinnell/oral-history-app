561d479979e21cecbac98aa9e6f277bc47d27f35
ochoaste <steffieocho@gmail.com>
2020-03-09 16:02:10 -0500

    Interview Submit Form

--- oral_history_app/config/routes.rb
+++ oral_history_app/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get  '/help',    to: 'pages#help'
   get  '/about',   to: 'pages#about'
   get  '/contact', to: 'pages#contact'
+  get  '/interview_signup', to: 'pages#interview_signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
